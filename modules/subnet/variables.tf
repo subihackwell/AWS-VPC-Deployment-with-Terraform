53fd0c02e7b164867148c5ac64e1a6460a2ad46a
subihackwell <lostinhacking123@gmail.com>
2025-08-23 10:01:35 -0400

    Create variables.tf

---
+++ modules/subnet/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "VPC ID for the subnet"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the subnet"
+  type        = string
+}
