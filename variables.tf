9f3a09b9b316f3d2010add7e2f424aba09a8a343
subihackwell <lostinhacking123@gmail.com>
2025-08-23 09:59:06 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-0c94855ba95c71c99"
+}
+
+variable "web_sg_id" {
+  description = "Security group ID for EC2 instance"
+  type        = string
+}
